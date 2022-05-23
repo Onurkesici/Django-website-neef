793d6b0d317fb1877379ffcc9f9c78d47faf639b
Onur KESİCİ <101974944+Onurkesici@users.noreply.github.com>
2022-05-23 18:24:37 +0300

    NEEF

---
+++ catalog/static/js/about-contact.js
@@ -0,0 +1,30 @@
+
+window.sr=ScrollReveal();
+
+sr.reveal(".anime-left",{
+    origin:"left",
+    duration:1000,
+    distance:"25rem",
+    delay:300
+});
+
+sr.reveal(".anime-right",{
+    origin:"right",
+    duration:1000,
+    distance:"25rem",
+    delay:600
+});
+
+sr.reveal(".anime-top",{
+    origin:"top",
+    duration:1000,
+    distance:"25rem",
+    delay:600
+});
+
+sr.reveal(".anime-bottom",{
+    origin:"bottom",
+    duration:1000,
+    distance:"25rem",
+    delay:600
+});
---
+++ catalog/static/js/portfolio.js
@@ -0,0 +1,9 @@
+
+ScrollReveal().reveal('.ani1',{delay:250});
+ScrollReveal().reveal('.ani2',{delay:500});
+ScrollReveal().reveal('.ani3',{delay:750});
+ScrollReveal().reveal('.ani4',{delay:1000});
+ScrollReveal().reveal('.ani5',{delay:1250});
+ScrollReveal().reveal('.ani6',{delay:1500});
+ScrollReveal().reveal('.ani7',{delay:1750});
+ScrollReveal().reveal('.ani8',{delay:2000});
